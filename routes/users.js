89bd3d144758297b9a405286420eb803f85eeefc
rpinto <rachel.lea.pinto@gmail.com>
2018-07-16 12:15:56 -0700

    Create a POST /api/users endpoint, validated all fields present, all are strings

---
+++ routes/users.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+
+const router = express.Router();
+
+//validate and cleanup the values supplied for username and password
+//all fields must exist
+const requiredFields = ['fullname', 'username', 'password'];
+const missingField = requiredFields.find(field => !(field in req.body));
+
+if (missingField) {
+  return res.status(422).json({
+    code: 422,
+    reason: 'ValidationError',
+    message: 'Missing field',
+    location: missingField
+  });
+};
+
+//all fields must be a string
+const stringFields = ['fullname', 'username', 'password'];
+const nonStringField = stringFields.find(
+  field => field in req.body && typeof req.body[field] !== 'string'
+);
+
+if (nonStringField) {
+  return res.status(422).json({
+    code: 422,
+    reason: 'ValidationError',
+    message: 'Incorrect field type: expected string',
+    location: nonStringField
+  });
+}
+//Create User ------ POST ENDPOINT ------
+router.post('/users', (req, res) => {
+  return User
+    .create({
+      fullname,
+      username,
+      password
+    })
+    .then(user => {
+      return res.status(201).location(`/api/users/${user.id}`).json(user.serialize());
+    });
+});
